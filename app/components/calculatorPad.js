b7991c70085cc47d1ce2d120a653d4635f717a2e
httpNick <nick.b.duncan@gmail.com>
2016-04-25 00:11:19 -0700

    added number pngs and rendered into a flexbox listview.

--- app/components/calculatorPad.js
+++ app/components/calculatorPad.js
@@ -1,6 +1,8 @@
 import React, {
   View,
+  ListView,
   Text,
+  Image,
   TouchableHighlight,
   Component,
   StyleSheet,
@@ -8,17 +10,37 @@ import React, {
   TouchableNativeFeedback
 } from 'react-native'
 
-class CalculatorPad extends Component {
+
+var CalculatorPad = React.createClass({
+
+  getInitialState() {
+    var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    return {
+      dataSource: ds.cloneWithRows(numbers),
+    };
+  },
 
   render() {
     return(
-      <View>
-        {this.generateAllButtons()}
-      </View>
+      <ListView
+        contentContainerStyle={styles.list}
+        dataSource={this.state.dataSource}
+        renderRow={this._renderRow}
+      />
     )
-  }
+  },
 
-  numPadButton = (number) => {
+  _renderRow(rowData, sectionID, rowID) {
+    return (
+      <TouchableNativeFeedback>
+          <View style={styles.row}>
+            <Image source={numbers[rowID]} style={styles.calcButton} />
+          </View>
+      </TouchableNativeFeedback>
+    )
+  },
+
+  numPadButton(number) {
     var TouchableElement = TouchableHighlight;
     if (Platform.OS === 'android') {
       TouchableElement = TouchableNativeFeedback;
@@ -32,29 +54,55 @@ class CalculatorPad extends Component {
       </View>
     </TouchableElement>
     )
-  };
+  },
 
-  generateAllButtons = () => {
+  generateAllButtons() {
     let i;
     let buttons = [];
     for (i = 0; i < 10; i++) {
       buttons.push(this.numPadButton(i));
     }
     return buttons;
-  };
+  }
 
-}
+});
 
 const styles = StyleSheet.create({
 
-  button : {
-    marginBottom: 7,
-    backgroundColor: 'blue',
+  list: {
+    justifyContent: 'center',
+    flexDirection: 'row',
+    flexWrap: 'wrap'
   },
 
-  buttonText : {
-    textAlign : 'center'
+  row: {
+    justifyContent: 'center',
+    padding: 5,
+    margin: 10,
+    width: 100,
+    height: 100,
+    alignItems: 'center',
+    borderRadius: 5,
+    borderColor: '#CCC'
+  },
+
+  calcButton : {
+    height: 64,
+    width: 64
   }
 });
+
+var numbers = [
+  require('../images/number-0.png'),
+  require('../images/number-1.png'),
+  require('../images/number-2.png'),
+  require('../images/number-3.png'),
+  require('../images/number-4.png'),
+  require('../images/number-5.png'),
+  require('../images/number-6.png'),
+  require('../images/number-7.png'),
+  require('../images/number-8.png'),
+  require('../images/number-9.png')
+];
 
 export default CalculatorPad
